4d0d40d91ec60ced1b7fffd75844a759b8c7ebfc
OlyaOlya <olia-ulyanickaya@mail.ru>
2017-11-25 22:55:27 +0200

    start page

--- valentino/views.py
+++ valentino/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, TemplateView
+from redis import Redis
+# from .models import Product
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'valentino/index.html', {})
+
+
