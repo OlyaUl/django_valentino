a4dc6ea5367c92b9f1ff6b1b2bf16d4d4d5eaea6
OlyaOlya <olia-ulyanickaya@mail.ru>
2017-11-27 11:04:01 +0200

    data in table

--- valentino/urls.py
+++ valentino/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     # url(r'^index/$', views.index, name='index'),
     url(r'^$', views.SpiderView.as_view(), name='index'),
+    url(r'^data/$', views.all_data, name='data'),
+
 ]
--- valentino/views.py
+++ valentino/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from redis import Redis
 from django.views.generic import TemplateView
+from .models import Product
 
 
 class SpiderView(TemplateView):
@@ -13,6 +14,10 @@ class SpiderView(TemplateView):
         redis.lpush('{0}:start_urls'.format(spider), url1)
         return redirect('index')
 
+
+def all_data(request):
+    all = Product.objects.all()
+    return render(request, 'valentino/data.html', {'products': all})
 
 
 # def index(request):
